7bb56be4f8ba67a36371a45d4655bec8e6c2b729
er1009 <97550175+er1009@users.noreply.github.com>
2024-03-01 09:53:10 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/gretelai/gretel-synthetics.git
+cd gretel-synthetics
+pip uninstall tensorflow-estimator
+pip install tensorflow-estimator==2.8
+pip install gretel-synthetics
+cd..
+git clone https://github.com/waico/tsad.git
